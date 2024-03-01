678c5e2c2bde4f46dccf57b52a557ff9025d68b5
Yalcin <bilgekagan902@gmail.com>
2024-03-01 11:05:41 +0300

    methot taimlama

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace methodTanimlama{
+    class Program{
+        public static void Main(string[] args){
+            int a = 3, b = 4;
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+            
+             Metotlar mt = new Metotlar();
+             mt.EkranaYazdir(Convert.ToString(sonuc));
+
+             int sonuc2 = mt.ArttirVeTopla(ref a,ref b);
+
+        }
+        static int Topla(int a,int b){
+            return a+b;
+        }
+    }
+        
+     class Metotlar{
+        public void EkranaYazdir(string veri){
+            Console.WriteLine(veri);
+        }
+        public int ArttirVeTopla(ref int a ,ref int b){
+            a += 1;
+            b += 1;
+            return a + b;
+        }
+    }
+}
